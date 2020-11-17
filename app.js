c68f52f8fe539d95327c2838da0e27782238121f
Kevin Basele <kevinbasele@hotmail.se>
2020-11-17 10:12:00 +0100

    skapat träff effekt

--- app.js
+++ app.js
@@ -30,12 +30,24 @@ class face{
     smack(){
         const head = document.querySelectorAll(".img")
         const wrapper = document.querySelector(".wrapper")
-        console.log(wrapper)
         
         for(let current of head){
+            let count = 0
             wrapper.addEventListener("click", (e)=>{
                 if(e.target === current) {
-                    console.log("click")}
+                    count++ 
+                    console.log(count)
+                    if(count==1){
+                        current.style.background=`url("/pictures/kevinsad.png") no-repeat`
+                        current.style.backgroundSize="100%"
+                        current.style.width="150px"
+                    }
+                    if(count==2){
+                        current.style.background=`url("/pictures/kevinmad.png") no-repeat`
+                        current.style.backgroundSize="100%"
+                        current.style.width="150px"
+                    }
+                }
             })
         }
     }
@@ -43,23 +55,35 @@ class face{
 
 class hammer{
     constructor(){
-        this.moveHammer()
+        this.mouse()
+
     }
 
+    mouse(){
+        const wrapper = document.querySelector(".wrapper")
+
+        wrapper.addEventListener("mousemove", this.moveHammer)
+        window.addEventListener("click", this.slamHammer)
+
+        
+
+    }
     moveHammer(e){
         const stretch = 100;
-        const body = document.getElementsByTagName("body")
         const hammer = document.querySelector("#hammer")
 
-        const {offsetWidth: width, offsetheight: height} = body
-        let {offsetX: x, offsetY: y} = e; 
+        hammer.style.top = e.pageY + "px";
+        hammer.style.left = e.pageX + "px";
 
-        const xStretch = Math.round((x /width * stretch) - (stretch / 2)); 
-        const yStretch = Math.round((y /width * stretch) - (stretch / 2)); 
+    }
 
-          
+    slamHammer(){
+        const hammer = document.querySelector("#hammer")
 
-
+        hammer.style.animation ="slam 0.14s ease"
+            setTimeout(() => {
+                hammer.style.removeProperty("animation")
+            }, 140);
     }
 
 }
